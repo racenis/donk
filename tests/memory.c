c38d2393aa6ef3315ea8fbf17aa10ea57c176119
racenis <martinsrasaolaslasa@gmail.com>
2026-05-02 20:33:13 +0300

    Added basic memory test

--- tests/memory.c
+++ tests/memory.c
@@ -1,12 +1,20 @@
-/* tests/memory.c -- unit tests fow memowy uwu >_< */
 #define UTEST_IMPLEMENTATION
 #include "utest.h"
 
 #include "memory.h"
 
-UTEST(memory, placeholder) {
-    /* TODO: add weaw tests hewe uwu >_< */
-    ASSERT_EQ(1, 1);
+UTEST(memory, default_malloc_delete) {
+	char* data = donk_malloc(23);
+	
+	ASSERT_NE(data, NULL);
+	
+	// these shouldn't crash
+	data[0] = 'A';
+	data[22] = 'B';
+	
+	donk_free(data);
+	
+	ASSERT_TRUE(1);
 }
 
 UTEST_MAIN()
