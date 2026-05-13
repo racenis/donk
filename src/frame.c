098b3d8b4bc264ad2c23e5241ee7330538deee32
racenis <martinsrasaolaslasa@gmail.com>
2026-05-13 04:51:11 +0300

    Added image frame

--- src/frame.c
+++ src/frame.c
@@ -1,5 +1,8 @@
 #include "frame.h"
 
+#include <string.h>
+#include <stdlib.h>
+
 /*
 	TODO: IMPLEMENT FRAME DECODING AND ENCODING
 	
@@ -13,3 +16,26 @@
 	
 	Maybe we will also have some stream metadata and coding tables.
 */
+
+
+
+
+void donk_frame_image_make(donk_frame_t* frame, int width, int height) {
+	memset(frame, 0, sizeof(donk_frame_t));
+	frame->type = DONK_FRAME_IMAGE;
+	frame->image.width = width;
+	frame->image.height = height;
+	frame->image.pixels = malloc(width * height * 3);
+}
+
+void donk_frame_yeet(donk_frame_t* frame) {
+	switch (frame->type) {
+		DONK_FRAME_IMAGE:
+			free(frame->image.pixels);
+			break;
+		DONK_FRAME_AUDIO:
+			break;
+	}
+	
+	memset(frame, 0, sizeof(donk_frame_t));
+} 
--- src/frame.h
+++ src/frame.h
@@ -1,6 +1,34 @@
 #ifndef DONK_FRAME_H
 #define DONK_FRAME_H
 
+enum {
+	DONK_FRAME_IMAGE,
+	DONK_FRAME_AUDIO
+};
 
+typedef struct {
+	int type;
+	
+	int channel;
+	int timestamp;
+	
+	union {
+		struct {
+			short* samples;
+			int sample_count;
+		} audio;
+		struct {
+			int width;
+			int height;
+			
+			unsigned char* pixels;
+		} image;
+	};
+} donk_frame_t;
+
+void donk_frame_audio_make(donk_frame_t* frame, int samples); 
+void donk_frame_image_make(donk_frame_t* frame, int width, int height); 
+
+void donk_frame_yeet(donk_frame_t* frame); 
 
 #endif // DONK_FRAME_H
